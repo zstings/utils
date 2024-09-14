d1cb6d24553cfd4f0944660490d19cddad038cce
qinzhongting <qinzhongting@outlook.com>
2024-09-14 11:46:10 +0800

    【优化】assign函数，优化ts类型推导正确

--- src/object/assign.ts
+++ src/object/assign.ts
@@ -1,10 +1,16 @@
 import isEmptyObject from "@/verify/isEmptyObject"
+import isObject from "@/verify/isObject"
+
+// 将元组类型转成对象类型的工具类型
+type MergeTuple<T extends any[]> = T extends [infer F, ...infer R] ? Omit<F, keyof MergeTuple<R>> & MergeTuple<R> : {};
+type Merge<T, U> = Omit<T, keyof U> & U;
 
 /**
  * 合并对象
  * @param target 目标对象，被合并的对象
  * @param sources 源对象，可以多个
  * @return 目标对象
+ * @throws target参数必须是对象  target参数不是对象时触发
  * @category 对象Object
  * @example
  * 对象合并效果与Object.assign一致
@@ -12,7 +18,8 @@ import isEmptyObject from "@/verify/isEmptyObject"
  * assign({a: 1, c: 3}, {c: 5}) // => {a: 1, c: 5}
  * ```
  */
-export default function assign(target: Record<string, any>, ...sources: Record<string, any>[]): Record<string, any> {
-  if (isEmptyObject(target)) return {}
-  return Object.assign(target, ...sources)
+export default function assign<T extends Record<string, any>, U extends Record<string, any>[]>(target: T, ...sources: U) {
+  if (!isObject(target)) throw 'target参数必须是对象'
+  if (isEmptyObject(target)) return {} as keyof T extends never ? {} : Merge<T, MergeTuple<U>>
+  return Object.assign(target, ...sources) as keyof T extends never ? {} : Merge<T, MergeTuple<U>>
 }
