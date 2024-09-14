395c2be21316cc1d840940eb1d8edb1e0c415041
qinzhongting <qinzhongting@outlook.com>
2024-09-14 14:18:17 +0800

    【优化】omit函数，优化ts类型推导正确

--- src/object/omit.ts
+++ src/object/omit.ts
@@ -1,9 +1,10 @@
 import deepClone from "@/util/deepClone"
+import isObject from "@/verify/isObject";
 
 /**
  * 删除指定对象的指定属性
- * @param object 指定对象
- * @param keys 指定属性
+ * @param target 指定对象
+ * @param keys 由需要删除的属性组成的数组，不传时为[]
  * @return 新的对象
  * @category 对象Object
  * @example
@@ -11,10 +12,12 @@ import deepClone from "@/util/deepClone"
  * omit({a: 1, b: 2, c: 3}, ['a', 'c']) // => {b: 2}
  * ```
  */
-export default function omit(object: Record<string, any>, keys: string[] = []): Record<string, any> {
-  const _object = deepClone(object)
-  Object.keys(_object).forEach(item => {
-    if (keys.includes(item)) delete _object[item]
-  })
-  return _object
+// 函数重载声明
+export default function omit<T extends Record<string, any>>(target: T): T;
+export default function omit<T extends Record<string, any>, U extends (keyof T)[]>(target: T, keys: U): Omit<T, U[number]>;
+export default function omit<T extends Record<string, any>, U extends (keyof T)[]>(target: T, keys?: U) {
+  if (!isObject(target)) throw 'target参数必须是对象'
+  const _target = deepClone(target)
+  ;(keys || []).forEach(key => delete _target[key])
+  return _target
 }
