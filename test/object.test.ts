d0da043ab7b93bbd2bb20e868f89eb04b077ca0f
qinzhongting <qinzhongting@outlook.com>
2024-09-14 11:46:40 +0800

    【测试】assign函数单元测试

--- test/object.test.ts
+++ test/object.test.ts
@@ -1,5 +1,25 @@
 import { expect, it, describe } from 'vitest'
-import { assignMin } from '@/index'
+import { assign, assignMin } from '@/index'
+
+describe('assign', () => {
+  it('测试基本的合并功能', () => {
+    expect(assign({ a: 1, b: 2 }, { b: 3, c: 4 }, { d: 5 })).toEqual({ a: 1, b: 3, c: 4, d: 5 });
+    expect(assign({ a: 1, b: 2 }, { b: 3, c: 4 }, { d: 5, b: '2'})).toEqual({ a: 1, b: '2', c: 4, d: 5 });
+    expect(assign({ a: 1, b: 2 })).toEqual({ a: 1, b: 2 });
+  });
+
+  it('测试被合并对象为空的情况', () => {
+    expect(assign({}, { a: 1 })).toEqual({});
+  });
+
+  it('测试被合并对象不是对象的情况', () => {
+    expect(() => assign(1 as any, { a: 1 })).toThrow('target参数必须是对象');
+  });
+
+  it('测试当源对象中有与目标相同的键时值被覆盖的情况', () => {
+    expect(assign({ a: 1 }, { a: 2, b: 3 }, { b: 4, c: 5 })).toEqual({ a: 2, b: 4, c: 5 });
+  });
+});
 
 describe('assignMin', () => {
   it('测试基本的合并功能', () => {
