96158960d4a3a7d4cfe41276231723c4348e8b6f
qinzhongting <qinzhongting@outlook.com>
2024-09-14 14:18:45 +0800

    【测试】omit函数单元测试

--- test/object.test.ts
+++ test/object.test.ts
@@ -1,5 +1,5 @@
 import { expect, it, describe } from 'vitest'
-import { assign, assignMin } from '@/index'
+import { assign, assignMin, omit } from '@/index'
 
 describe('assign', () => {
   it('测试基本的合并功能', () => {
@@ -38,5 +38,20 @@ describe('assignMin', () => {
 
   it('测试当源对象中有与目标相同的键时值被覆盖的情况', () => {
     expect(assignMin({ a: 1 }, { a: 2, b: 3 }, { b: 4, c: 5 })).toEqual({ a: 2 });
+  });
+});
+
+describe('omit', () => {
+  it('测试基本的删除功能', () => {
+    expect(omit({ a: 1, b: 2 }, ['a'])).toEqual({ b: 2 });
+    expect(omit({ a: 1, b: 2 }, ['a', 'b'])).toEqual({});
+  });
+
+  it('测试目标对象为空的情况', () => {
+    expect(omit({})).toEqual({});
+  });
+
+  it('测试目标对象不是对象的情况', () => {
+    expect(() => omit(1 as any)).toThrow('target参数必须是对象');
   });
 });
